499bfb8829f80b8b6ecc2f04d07cbe9afd556010
sumit23802380 <sumitjangid1692153@gmail.com>
2023-11-30 00:39:46 +0530

    Refactored : AddressBook Class Method

--- src/com/bridgelabz/addressbookmanagementsystem/AddressBook.java
+++ src/com/bridgelabz/addressbookmanagementsystem/AddressBook.java
@@ -7,6 +7,9 @@ import java.util.Scanner;
     @desc : Address Book Class
  */
 public class AddressBook {
+    private static final int ADD_CONTACT=1;
+    private static final int EDIT_CONTACT=2;
+    private static final int CLOSE_ADDRESS_BOOK =0;
     ArrayList<Contact> contactList;
     /*
         @desc : Constructor of AddressBook Class
@@ -14,7 +17,29 @@ public class AddressBook {
     public AddressBook(){
         this.contactList = new ArrayList<>();
     }
-    public void addContact(){
+    public void open(){
+        System.out.println("Select options : ");
+        System.out.println("Press 1 if want to add a new contact");
+        System.out.println("Press 2 if you want to edit an existing contact");
+        System.out.println("Press 0 To exit");
+        Scanner sc = new Scanner(System.in);
+        int pressedOption = sc.nextInt();
+        switch (pressedOption){
+            case ADD_CONTACT :
+                this.addContact();
+                break;
+            case EDIT_CONTACT:
+                this.editContact();
+                break;
+            case CLOSE_ADDRESS_BOOK:
+                return;
+            default:
+                System.out.println("Please press valid button");
+        }
+        open();
+    }
+    private void addContact(){
+        System.out.println("Please provide all the details to add a new Contact:");
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter First Name");
         String firstName = sc.next();
@@ -34,5 +59,10 @@ public class AddressBook {
         String email = sc.next();
         Contact contact = new Contact(firstName , lastName , address , city , state , zip , phoneNumber , email);
         contactList.add(contact);
+    }
+    private void editContact(){
+        System.out.println("Edit the contact");
+        System.out.println("Enter existing First Name");
+        System.out.println("Enter existing Last Name");
     }
 }
--- src/com/bridgelabz/addressbookmanagementsystem/AddressBookMain.java
+++ src/com/bridgelabz/addressbookmanagementsystem/AddressBookMain.java
@@ -2,9 +2,10 @@ package com.bridgelabz.addressbookmanagementsystem;
 import java.util.Scanner;
 
 public class AddressBookMain {
+
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book");
         AddressBook addressBook = new AddressBook();
-        addressBook.addContact();
+        addressBook.open();
     }
 }
