b5b3a9814b3ff4ef9dc84218ddad294d1f4268b3
sumit23802380 <sumitjangid1692153@gmail.com>
2023-11-30 00:14:37 +0530

    Refactored : UC1 add contact

--- src/com/bridgelabz/addressbookmanagementsystem/AddressBook.java
+++ src/com/bridgelabz/addressbookmanagementsystem/AddressBook.java
@@ -1,6 +1,8 @@
 package com.bridgelabz.addressbookmanagementsystem;
 
 import java.util.ArrayList;
+import java.util.Scanner;
+
 /*
     @desc : Address Book Class
  */
@@ -14,5 +16,26 @@ public class AddressBook {
     }
     public void addContact(Contact contact){
         this.contactList.add(contact);
+    }
+    public void takeContactInput(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Name");
+        String firstName = sc.next();
+        System.out.println("Enter Last Name");
+        String lastName = sc.next();
+        System.out.println("Enter Address");
+        String address = sc.next();
+        System.out.println("Enter City");
+        String city = sc.next();
+        System.out.println("Enter State");
+        String state = sc.next();
+        System.out.println("Enter zipCode");
+        String zip = sc.next();
+        System.out.println("Enter Phone Number");
+        String phoneNumber = sc.next();
+        System.out.println("Enter Email");
+        String email = sc.next();
+        Contact contact = new Contact(firstName , lastName , address , city , state , zip , phoneNumber , email);
+        contactList.add(contact);
     }
 }
--- src/com/bridgelabz/addressbookmanagementsystem/AddressBookMain.java
+++ src/com/bridgelabz/addressbookmanagementsystem/AddressBookMain.java
@@ -5,24 +5,6 @@ public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book");
         AddressBook addressBook = new AddressBook();
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter First Name");
-        String firstName = sc.next();
-        System.out.println("Enter Last Name");
-        String lastName = sc.next();
-        System.out.println("Enter Address");
-        String address = sc.next();
-        System.out.println("Enter City");
-        String city = sc.next();
-        System.out.println("Enter State");
-        String state = sc.next();
-        System.out.println("Enter zipCode");
-        String zip = sc.next();
-        System.out.println("Enter Phone Number");
-        String phoneNumber = sc.next();
-        System.out.println("Enter Email");
-        String email = sc.next();
-        Contact contact = new Contact(firstName , lastName , address , city , state , zip , phoneNumber , email);
-        addressBook.addContact(contact);
+        addressBook.takeContactInput();
     }
 }
