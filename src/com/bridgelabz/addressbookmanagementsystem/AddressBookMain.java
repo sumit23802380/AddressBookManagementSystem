93c0f0613ebabff02a3a90132ef2ea7b111ee99b
sumit23802380 <sumitjangid1692153@gmail.com>
2023-11-29 23:03:50 +0530

    Added : AddressBookMain Class

---
+++ src/com/bridgelabz/addressbookmanagementsystem/AddressBookMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.addressbookmanagementsystem;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book");
+    }
+}
